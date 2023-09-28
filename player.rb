184c1f0de41ded1c334ccc667cd3d2e5474f4c29
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-09-28 19:38:18 +0300

    Add Player.rb class

---
+++ player.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'deck'
+
+class Player
+  attr_reader :name, :account, :deck, :score
+
+  START_ACCOUNT = 100
+
+  def initialize(name = 'No_name', account = START_ACCOUNT)
+    @name = name.to_s
+    @account = account.to_i
+    @deck = Deck.new(1)
+    @score = count_score
+  end
+
+  def make_bet(bet_size)
+    @account -= bet_size unless (@account - bet_size).negative?
+  end
+
+  def request_card(number, dealer)
+    number.times { deck.add_card(dealer.give_card) }
+    @score = count_score
+  end
+
+  def count_cards
+    deck.length
+  end
+
+  def increase_account(amount)
+    @account += amount
+  end
+
+  private
+
+  attr_writer :name, :account, :deck, :score
+
+  def count_score
+    return 0 if @deck.empty?
+
+    score = 0
+    count_aces = 0
+    @deck.cards.each do |card|
+      count_aces += 1 if card.rank == 'T'
+      score += if (card.rank == 'T') && (count_aces > 1)
+                 1
+               else
+                 card.value
+               end
+    end
+    score
+  end
+end
