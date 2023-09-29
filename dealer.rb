c890e469e673c1b9f3bbd9e339fed295e4ae7b93
Kruglov Alexey <kruglov_aleksei@mail.ru>
2023-09-30 00:35:40 +0300

    Add Dealer.rb class

---
+++ dealer.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative 'player'
+
+class Dealer < Player
+  attr_reader :game_deck
+
+  DEFAULT_NAME = 'Dealer'
+
+  def initialize(name = nil, account = nil)
+    super
+    @name = DEFAULT_NAME
+    @game_deck = Deck.new(0)
+    @game_deck.shuffle_deck!
+  end
+
+  def give_card
+    @game_deck.take_out_card!
+  end
+
+  private
+
+  attr_writer :game_deck
+
+  def shuffle_deck!
+    @game_deck.shuffle!
+  end
+end
--- deck.rb
+++ deck.rb
@@ -19,12 +19,22 @@ class Deck
   end
   # rubocop:enable Style/GuardClause
 
-  def add_card(card)
+  def add_card!(card)
     @cards.push(card) if card.instance_of?(Card)
   end
 
-  def take_out_card
+  def take_out_card!
     @cards.shift
+  end
+
+  def shuffle!
+    i = cards.length
+    while i > 1
+      i -= 1
+      # 0 <= j <= i-1
+      j = rand(i)
+      cards[j], cards[i] = cards[i], cards[j]
+    end
   end
 
   private
